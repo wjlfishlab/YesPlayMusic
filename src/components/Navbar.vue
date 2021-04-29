b95cc3bbb3e6b4fb4fbdbe3803a7ad016962e2d9
wjlfishlab <83385307+wjlfishlab@users.noreply.github.com>
2021-04-29 11:31:01 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -78,11 +78,6 @@
         <svg-icon icon-class="logout" />
         {{ $t('library.userProfileMenu.logout') }}
       </div>
-      <hr />
-      <div class="item" @click="toGitHub">
-        <svg-icon icon-class="github" />
-        {{ $t('nav.github') }}
-      </div>
     </ContextMenu>
   </div>
 </template>
